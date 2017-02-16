c416ae564467906df999add6b8623a59fd776227
Ricardo Nabinger Sanchez <rnsanchez@wait4.org>
2017-02-16 11:53:17 -0200

    fish: use full path for calling fortune

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,6 @@
 # Mimics Slackware's /etc/profile.d/bsd-games-login-fortune.csh
 function fish_greeting
 	echo ""
-	fortune fortunes fortunes2 linuxcookie
+	/usr/games/fortune fortunes fortunes2 linuxcookie
 	echo ""
 end
